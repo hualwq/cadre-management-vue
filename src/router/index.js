0ba1f9d25586caef9e5bff3d3cacb1cb7e7786a5
laowang <1598273166@qq.com>
2025-06-17 13:36:57 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,14 @@ import CadreHome from '../components/CadreHome.vue';
 import PostAssessment from '../components/PostAssessment.vue';
 import PostCadreposition from '../components/PostCadreposition.vue';
 import PostCadreInfo from '../components/PostCadreInfoForm.vue';
+import GetAssessmentbyid from '../components/GetAssessmentbyid.vue';
+import GetAssessmentbypage from '../components/GetAssessment.vue';
+import GetCadreinfobyid from '../components/GetCadreinfobyid.vue';
+import GetCadreinfobypage from '../components/GetCadreinfo.vue';
+import GetPositionhistorybyid from '../components/GetPositionhistorybyid.vue';
+import GetPositionhistorybypage from '../components/GetPositionhistory.vue';
+import AdminHome from '../components/AdminHome'
+
 
 const routes = [
   {
@@ -21,6 +29,44 @@ const routes = [
       const role = localStorage.getItem('role');
       if (role === 'cadre') {
         return CadreHome;
+      } else {
+        // 可添加其他角色的默认页面
+        return Login;
+      }
+    },
+    children: [
+      {
+        path: 'get-assessmentlist',
+        component: GetAssessmentbypage
+      },
+      {
+        path: 'get-cadreinfolist',
+        component: GetCadreinfobypage
+      },
+      {
+        path: 'get-positionhistorylist',
+        component: GetPositionhistorybypage
+      },
+      {
+        path: 'get-assessment',
+        component: GetAssessmentbyid
+      },
+      {
+        path: 'get-cadreinfo',
+        component: GetCadreinfobyid
+      },
+      {
+        path: 'get-positionhistory',
+        component: GetPositionhistorybyid
+      }
+    ]
+  },
+  {
+    path: '/home',
+    component: () => {
+      const role = localStorage.getItem('role');
+      if (role === 'admin') {
+        return AdminHome;
       } else {
         // 可添加其他角色的默认页面
         return Login;
@@ -48,21 +94,21 @@ const router = createRouter({
   routes
 });
 
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
-  const role = localStorage.getItem('role');
+// router.beforeEach((to, from, next) => {
+//   const token = localStorage.getItem('token');
+//   const role = localStorage.getItem('role');
 
-  if (token || to.path === '/login') {
-    if (to.path.includes('/home') && role!== 'cadre') {
-      alert('无权访问');
-      next('/login');
-    } else {
-      next();
-    }
-  } else {
-    alert('无权访问');
-    next('/login');
-  }
-});
+//   if (token || to.path === '/login') {
+//     if (to.path.includes('/home') && role!== 'cadre') {
+//       alert('无权访问');
+//       next('/login');
+//     } else {
+//       next();
+//     }
+//   } else {
+//     alert('无权访问');
+//     next('/login');
+//   }
+// });
 
 export default router;
